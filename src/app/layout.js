a2592a783bf9f6908f915245abccd681e2de87d7
sergiotechx <sergiotechx@yahoo.com>
2023-09-27 16:58:31 -0500

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ const roboto = Roboto_Serif({
   weight: ["100", "200", "300", "400"],
 });
 
- /*const metadata = {
+ /*const metadata = { 
   title: "Found Pets",
   description: "Made with love 🐶",
 };*/
